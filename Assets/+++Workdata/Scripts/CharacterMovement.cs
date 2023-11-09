5b531dcedfc30cba26e2497b8e7ae863049b26d2
DeathGZ <rafael20031209@gmail.com>
2023-11-09 12:17:10 +0100

    9.11.2023
    Jump.roll

--- Assets/+++Workdata/Scripts/CharacterMovement.cs
+++ Assets/+++Workdata/Scripts/CharacterMovement.cs
@@ -5,17 +5,22 @@ using System.Collections.Generic;
 
 using UnityEngine.InputSystem;
 using static UnityEngine.LightAnchor;
+using static UnityEngine.InputSystem.InputAction;
 
 public class CharacterMovement : MonoBehaviour
 {
     private Player_InputActions inputActions;/// <summary>                                             /// input action declaration                                             /// </summary>
     private InputAction moveAction;
     public InputAction jumpAction;
+    public LayerMask groundLayer;
     public float rollDistance = 10;
     public InputAction blockAction;
     public InputAction rollAction;
     public InputAction attackAction;
     public bool facingRight = true;
+    public Vector3 feetSize;
+    public bool isGrounded;
+    public Vector3 feetPosition;
     public Vector2 moveInput;
     public float jumpStrength = 10;
 
@@ -38,10 +43,10 @@ public class CharacterMovement : MonoBehaviour
     private void OnEnable()
     {
         inputActions.Enable();
-        attackAction.Enable();
+        attackAction.performed += Attack;
         blockAction.Enable();
         rollAction.Enable();
-        jumpAction.Enable();
+        jumpAction.performed += Jump;
     }
 
     private void Update()
@@ -62,8 +67,8 @@ public class CharacterMovement : MonoBehaviour
             Flip();
 
 
-        if (attackAction.triggered)
-            Attack();
+        //if (attackAction.triggered)
+           //Attack();
 
         if (blockAction.triggered)
             Block();
@@ -71,8 +76,8 @@ public class CharacterMovement : MonoBehaviour
         if (rollAction.triggered)
             Roll();
 
-        if (jumpAction.triggered)
-            Jump();
+        //if (jumpAction.triggered)
+            //Jump();
 
 
         if (facingRight == false && moveInput.x > 0)
@@ -85,15 +90,26 @@ public class CharacterMovement : MonoBehaviour
 
    void FixedUpdate()
     {
+        isGrounded = Physics2D.OverlapBox(gameObject.transform.position + feetPosition, feetSize, 0, groundLayer);
+
+
+
         //rb.velocity = new Vector2(moveInput.x * speed, moveInput.y * speed);
         rb.AddForce(moveInput.x * speed * Vector2.right);
     }
-  
 
-     void OnDisable()
+    private void LateUpdate()
+    {
+        anim.SetBool("isGrounded", isGrounded);
+
+    }
+
+
+
+    void OnDisable()
     {
         inputActions.Disable();
-        attackAction.Disable();
+        attackAction.performed += Attack;
         blockAction.Disable();
         rollAction.Disable();
         jumpAction.Disable();
@@ -110,10 +126,10 @@ public class CharacterMovement : MonoBehaviour
         scaler.x *= -1;
         transform.localScale = scaler;
     }
-    void Attack()
+    void Attack(CallbackContext ctx)
     {
         anim.SetTrigger("ActionTrigger");
-        anim.SetInteger("Actionid", 0);
+        anim.SetInteger("Actionid", 100);
     }
 
     void Block()
@@ -130,13 +146,16 @@ public class CharacterMovement : MonoBehaviour
         rb.velocity += new Vector2(transform.localScale.x, 0f) * rollDistance;
     }
 
-    void Jump()
+    void Jump(CallbackContext ctx)
     {
         anim.SetTrigger("ActionTrigger");
         anim.SetInteger("Actionid", 9);
         rb.velocity = new Vector2(rb.velocity.x, 0f);
         rb.velocity += Vector2.up * jumpStrength;
     }
-           
-     
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireCube(gameObject.transform.position + feetPosition, feetSize);
+    }
 }
